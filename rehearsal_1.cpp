16bc70f365c76cca0a39e86e2277c8166f730211
Shoukugeki02 <karn@eng.cmu.ac.th>
2020-01-10 17:11:42 +0700

    ข้อ1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+double X;
+string name;
+cout << "what is your name?\n";
+cin >> name;
+cout << "what is your GPA?\n";
+cin >> X;
+if(X>=3.50){
+cout << "InW Jrim!!! Jrim!!!\n";
+}
+else{
+cout << "try harder\n ";
+
+}
+ return 0;
+}
+
+
+
+    
+
+
+
 
